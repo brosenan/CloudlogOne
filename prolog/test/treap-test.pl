212748342beeaca21f3c6b46291d8755bc03d1a7
Boaz Rosenan <brosenan@gmail.com>
2015-04-07 20:25:23 +0300

    treap-rename-ops

--- prolog/test/treap-test.pl
+++ prolog/test/treap-test.pl
@@ -4,30 +4,30 @@
 test(create_empty) :-
 	treap:empty(_).
 
-test(lookup_returns_zero, [true(R == 0)]) :-
+test(get_returns_nil, [true(R == nil)]) :-
 	treap:empty(T0),
-	treap:lookup(T0, foo, R).
+	treap:get(T0, foo, R).
 
 test(add_and_retrieve_value, [true(R == 3)]) :-
 	treap:empty(T0),
-	treap:add(T0, foo, 1, 3, T1),
-	treap:lookup(T1, foo, R).
+	treap:set(T0, foo, 1, 3, T1),
+	treap:get(T1, foo, R).
 
 test(add_and_retrieve_two_values, [true([R1, R2] == [2, 3])]) :-
 	treap:empty(T0),
-	treap:add(T0, foo, 1, 2, T1),
-	treap:add(T1, bar, 2, 3, T2),
-	treap:lookup(T2, foo, R1),
-	treap:lookup(T2, bar, R2).
+	treap:set(T0, foo, 1, 2, T1),
+	treap:set(T1, bar, 2, 3, T2),
+	treap:get(T2, foo, R1),
+	treap:get(T2, bar, R2).
 
 test(left_rotation, [true(T2 = t(_, b, 2, 2, _))]) :-
 	treap:empty(T0),
-	treap:add(T0, a, 1, 3, T1),
-	treap:add(T1, b, 2, 2, T2).
+	treap:set(T0, a, 1, 3, T1),
+	treap:set(T1, b, 2, 2, T2).
 
 test(right_rotation, [true(T2 = t(_, a, 2, 2, _))]) :-
 	treap:empty(T0),
-	treap:add(T0, b, 1, 3, T1),
-	treap:add(T1, a, 2, 2, T2).
+	treap:set(T0, b, 1, 3, T1),
+	treap:set(T1, a, 2, 2, T2).
 
 :- end_tests(treap).
--- prolog/treap.pl
+++ prolog/treap.pl
@@ -1,33 +1,33 @@
-:- module(treap, [empty/1, lookup/3, add/5]).
+:- module(treap, [empty/1, get/3, set/5]).
 :- use_module(util).
 
 treap:empty(nil).
 
-treap:lookup(nil, _, 0).
-treap:lookup(t(L, K, _, V, R), K1, V1) :-
+treap:get(nil, _, nil).
+treap:get(t(L, K, _, V, R), K1, V1) :-
 	if(K = K1,
 	  V1 = V,
 	%else
 	  if(K1 @< K,
-	    treap:lookup(L, K1, V1),
+	    treap:get(L, K1, V1),
 	  %else
-	    treap:lookup(R, K1, V1))).
+	    treap:get(R, K1, V1))).
 
-treap:add(nil, K, W, V, t(nil, K, W, V, nil)).
-treap:add(t(L, K, W, V, R), K1, W1, V1, TOut) :-
+treap:set(nil, K, W, V, t(nil, K, W, V, nil)).
+treap:set(t(L, K, W, V, R), K1, W1, V1, TOut) :-
 	if(K == K1, (
 	  L2 = L,
 	  R2 = R,
 	  V2 is V + V1),
 	%else
 	  if(K1 @< K, (
-	    treap:add(L, K1, W1, V1, L2),
+	    treap:set(L, K1, W1, V1, L2),
 	    R2 = R,
 	    V2 = V,
 	    treap:rotateRight(t(L2, K, W, V2, R2), TOut)),
 	  %else
 	    (L2 = L,
-	    treap:add(R, K1, W1, V1, R2),
+	    treap:set(R, K1, W1, V1, R2),
 	    V2 = V,
 	    treap:rotateLeft(t(L2, K, W, V2, R2), TOut)))).
 
