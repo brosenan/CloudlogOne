0c30109935e03ffc79181e9385ee68b17bfb1031
Boaz Rosenan <brosenan@gmail.com>
2015-04-08 09:30:55 +0300

    dominates

---
+++ prolog/termcompare.pl
@@ -0,0 +1,23 @@
+:- module(termcompare, []).
+:- use_module(util).
+
+termcompare:dominates(X, Y) :-
+	if(var(X),
+	  true,
+	% else
+	  if(var(Y),
+	    fail,
+	  % else
+	    (X =.. [Func | XArgs],
+	    Y =.. [Func | YArgs],
+	    length(XArgs, Arity),
+	    length(YArgs, Arity),
+	    termcompare:dominatesList(XArgs, YArgs)))).
+
+termcompare:dominatesList([], []).
+termcompare:dominatesList([X | Xs], [Y | Ys]) :-
+	if(termcompare:dominates(X, Y),
+	  true,
+	% else
+	  (X == Y,
+	  termcompare:dominatesList(Xs, Ys))).
---
+++ prolog/test/termcompare-test.pl
@@ -0,0 +1,25 @@
+:- begin_tests(termcompare).
+:- use_module('../termcompare.pl').
+
+test(dominates_equals, []) :-
+	termcompare:dominates(a(b), a(b)).
+
+test(dominates_var_vs_ground, []) :-
+	termcompare:dominates(a(_), a(b)).
+
+test(dominates_ground_vs_var, []) :-
+	\+termcompare:dominates(a(b), a(_)).
+
+test(dominates_diff_after_first_var, []) :-
+	termcompare:dominates(a(_, 1), a(b, 2)).
+
+test(dominates_functor, []) :-
+	\+termcompare:dominates(a(X, 1), b(X, 1)).
+
+test(dominates_arity, []) :-
+	\+termcompare:dominates(a(X, 1), a(X, 1, 3)).
+
+test(dominates_nested, []) :-
+	termcompare:dominates(a(b(1, _), c), a(b(1, 2), _)).
+
+:- end_tests(termcompare).
