1c61b053fceb3802cac4bc970d07799cb36acdc3
Boaz Rosenan <brosenan@gmail.com>
2015-04-25 21:59:58 +0300

    PeerLocator.getServerFor

--- js/peerLocator.js
+++ js/peerLocator.js
@@ -3,17 +3,21 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var request = require('request');
 var $S = require('suspend'), $R = $S.resume;
+var NodeHash = require('./nodehash.js');
 
 var SUFFIX = '/_peerloc';
 
-module.exports = function(port, master) {
+module.exports = function(port, master, size) {
     var self = this;
+    size = size || 100;
+    this._nodehash = new NodeHash(size);
     this._app = express();
     this._port = port;
     this._master = master;
     this._peers = {};
     if(master) {
 	this._peers[master] = 1;
+	this._nodehash.addServer(master);
     }
     this._app.use(bodyParser.json());
     this._app.post(SUFFIX, function(req, res) {
@@ -21,6 +25,7 @@ module.exports = function(port, master) {
 	self._mergeResponse(req.body);
 	res.send(self._myPeers());
     });
+    this._server = null;
 };
 var clazz = module.exports.prototype;
 
@@ -29,15 +34,16 @@ clazz.app = function() {
 };
 
 clazz.run = $S.async(function*() {
-    yield this._app.listen(this._port, $R());
+    this._server = this._app.listen(this._port, $R());
+    yield; // Wait for the server to come up
     if(this._master) {
 	yield this._contactPeer(this._master, $R());
     }
 });
 
-//clazz.getServerFor = function(id) {
-//    return Object.keys(this._peers)[0];
-//};
+clazz.getServerFor = function(key) {
+    return this._nodehash.getServerFor(key);
+};
 
 clazz.knownPeers = function() {
     return Object.keys(this._peers);
@@ -48,6 +54,7 @@ clazz._mergeResponse = function(resp) {
     var unknown = [];
     Object.keys(resp.peers).forEach(function(peer) {
 	if(!(peer in self._peers)) {
+	    self._nodehash.addServer(peer);
 	    self._addPeer(peer);
 	    unknown.push(peer);
 	}
@@ -88,4 +95,8 @@ clazz._contactPeer = $S.async(function*(peer) {
 	self._contactPeer(other, $S.fork());
     });
     yield $S.join();
+});
+
+clazz.stop = $S.async(function*() {
+    yield this._server.close($R());
 });
--- test/peerLocator-test.js
+++ test/peerLocator-test.js
@@ -36,8 +36,31 @@ describe('PeerLocator', function(){
 	    assert.equal(l1.knownPeers().length, 3);
 	    assert.equal(l2.knownPeers().length, 3);
 	    assert.equal(l3.knownPeers().length, 3);
+	    yield l1.stop($R());
+	    yield l2.stop($R());
+	    yield l3.stop($R());
 	}));
-
+    });
+    describe('.getServerFor(key)', function(){
+	it('should return one of the server URLs for each key, consistently from each locator', $T(function*(){
+	    var master = 'http://localhost:4050';
+	    var l1 = new PeerLocator(4050, null, 100);
+	    yield l1.run($R());
+	    var l2 = new PeerLocator(4051, master, 100);
+	    yield l2.run($R());
+	    var count = 0;
+	    for(let i = 0; i < 100; i++) {
+		let key = 'foo' + i;
+		// The value returned must be consistent
+		assert.equal(l1.getServerFor(key), l2.getServerFor(key));
+		// Make sure the result is distributed
+		if(l1.getServerFor(key) === master) {
+		    count += 1;
+		}
+	    }
+	    assert(count > 0, count + ' > 0');
+	    assert(count < 100, count + ' < 100');
+	}));
     });
 
 });
