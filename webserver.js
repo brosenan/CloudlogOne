8445de32fcc2815d2899734a5057b40041a875e0
Boaz Rosenan <brosenan@gmail.com>
2015-04-25 18:35:14 +0300

    PeerLocator

--- webserver.js
+++ webserver.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello, ' + req.ip);
+    res.send('Hello, ' + req.ip.match(/[^:]*$/)[0]);
 });
 
 var server = app.listen(3000, function () {
